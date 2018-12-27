6c1e3f3be9001f9dcf68c5669e341eba36e9cacc
tusonggao <tusonggao@163.com>
2018-12-27 16:12:26 +0800

    first commit

---
+++ read_from_impala.py
@@ -0,0 +1,53 @@
+# dn01: 172.21.57.123
+# dn02: 172.21.57.124
+# dn03: 172.21.57.125
+# dn04: 172.21.57.126
+# dn05: 172.21.57.127
+
+# nm01: 172.21.57.128
+# dn02: 172.21.57.129
+
+###############################################################
+
+
+import json
+import time
+import pandas as pd
+
+def read_sql_script(sql_file):
+	with open(sql_file) as file:
+		sql_str = ' '.join([line.strip() for line in file])
+	return sql_str
+
+
+
+
+
+# with open("./111.json", 'r', encoding='UTF-8') as f:
+# with open('./wanzheng.json', 'r', encoding='UTF-8') as f:
+#     temp = json.loads(f.read())
+#     #print(temp)
+#     #print(temp['RECORDS'])
+#     print(len(temp['RECORDS']))
+#     print(temp['RECORDS'][1]['Id'])
+# print('hello world!')
+
+from sqlalchemy import create_engine
+
+start_t = time.time()
+
+conn_impala = create_engine('impala://172.21.57.127:21050')
+sql = 'show databases;'
+sql = read_sql_script('./sql_scripts/hive_sql_5.txt')
+print('sql is ', sql)
+df = pd.read_sql(sql, conn_impala)
+
+end_t = time.time()
+print('df.shape is', df.shape)
+
+print('df.head() is', df.head())
+print('read data cost time ', end_t-start_t)
+
+df.to_csv('./hive_sql_5_output.csv')
+
+
