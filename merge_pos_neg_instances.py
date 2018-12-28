c373d4ddeb3ee473815e2bc882e8c28cda4f725d
tusonggao <tusonggao@163.com>
2018-12-29 07:48:08 +0800

    udpate file

--- merge_pos_neg_instances.py
+++ merge_pos_neg_instances.py
@@ -1,26 +1,23 @@
 import numpy as np
 import pandas as pd
 
-seed = 42
-
+random_seed = 42
+np.random.seed(random_seed)
 
 df_pos = pd.read_csv('./data/hive_sql_pos_instances_data.csv')
 df_neg = pd.read_csv('./data/hive_sql_neg_instances_data_modified.csv')
-df_neg = df_neg.sample(n=600000, random_state=42)
+df_neg = df_neg.sample(n=600000)
 
 df_pos['y'] = 1
 df_neg['y'] = 0
 
-print('df_pos shape is ', df_pos.shape)
-print('df_pos head is ', df_pos.head(3))
-
-print('df_neg is ', df_neg.shape)
-print('df_neg head is ', df_neg.head(3))
-
-df_merged = 
+df_merged = pd.concat([df_pos, df_neg])
 
 print('df_pos shape is ', df_pos.shape)
 print('df_pos head is ', df_pos.head(3))
 
 print('df_neg is ', df_neg.shape)
 print('df_neg head is ', df_neg.head(3))
+
+print('df_merged is ', df_merged.shape)
+print('df_merged sample is ', df_merged.sample(10))
