c6178fc4fb5d4c9f1dda013c4b73c07db35919d4
tusonggao <tusonggao@163.com>
2019-01-13 20:34:24 +0800

    update file

--- get_final_results.py
+++ get_final_results.py
@@ -9,7 +9,11 @@ with open('F:/kefu_zhineng/sql_scripts_get_final_results/branch_code_map.txt', e
         branch_code_map[branch_code] = branch_code_chinese
 print(branch_code_map)
 
-df_results = pd.read_csv('./data/hive_sql_scripts_get_final_results_data.csv', 
+# df_results = pd.read_csv('./data/hive_sql_scripts_get_final_results_data.csv', 
+#                          dtype={'branch_code': int})
+# print(df_results.head(10))
+
+df_results = pd.read_csv('./data/hive_sql_scripts_get_final_results_new_data.csv', 
                          dtype={'branch_code': int})
 print(df_results.head(10))
 
@@ -18,17 +22,25 @@ print('df_results.columns.tolist() ', df_results.columns.tolist())
 # print('uniques() is ', df_results['branch_code'].value_counts())
 print('uniques() is ', df_results['branch_code'].unique())
 
-top = 500
+top = 3000
 df_results = df_results.iloc[:top]
+
 df_results['branch_code_chinese'] = df_results['branch_code'].map(branch_code_map)
-df_results = df_results[['buy_user_id', 'branch_code_chinese', 'branch_code', 'possibility']]
+df_results = df_results[['buy_user_id', 'orders_code', 'last_orders_date', 'last_order_cost', 
+                         'branch_code_chinese', 'branch_code', 'possibility']]
+
+# buy_user_id,branch_code,possibility,orders_code,last_orders_date,last_order_cost
 
 ## 改变dataframe的列名：
 df_results.rename(columns={'branch_code': '科室代码', 'branch_code_chinese': '科室名', 
-                           'buy_user_id': '客户编码', 'possibility': '模型输出概率'},
+                           'buy_user_id': '客户编码', 'possibility': '模型输出概率',
+                           'orders_code': '订单编号', 'last_orders_date': '最近购买时间',
+                           'last_order_cost': '购买金额'},
                   inplace=True)
 
 # df_results.to_csv('./data/final_results_data_processed.csv', index=False, sep='\t')
-df_results.to_excel('./data/final_results_data_processed.xlsx', index=False)
+
+df_results.to_excel('./data/final_results_data_processed_new.xlsx', index=False)
+df_results[:500].to_excel('./data/final_results_data_processed_new_top500.xlsx', index=False)
 
 
