7157306b4d986f21d8fb590d090ec0b155243d11
tusonggao <tusonggao@163.com>
2019-01-14 11:56:28 +0800

    update files

--- get_final_results.py
+++ get_final_results.py
@@ -5,7 +5,7 @@ branch_code_map = {}
 with open('F:/kefu_zhineng/sql_scripts_get_final_results/branch_code_map.txt', encoding='utf-8') as file:
     for line in file:
         branch_code_chinese, branch_code = line.strip().split()
-        branch_code = int(branch_code)
+        branch_code = branch_code.strip()
         branch_code_map[branch_code] = branch_code_chinese
 print(branch_code_map)
 
@@ -14,7 +14,7 @@ print(branch_code_map)
 # print(df_results.head(10))
 
 df_results = pd.read_csv('./data/hive_sql_scripts_get_final_results_new_data.csv', 
-                         dtype={'branch_code': int, 'orders_code': str})
+                         dtype={'branch_code': str, 'orders_code': str})
 print(df_results.head(10))
 
 print('df_results.columns.tolist() ', df_results.columns.tolist())
@@ -27,7 +27,7 @@ df_results = df_results.iloc[:top]
 
 df_results['branch_code_chinese'] = df_results['branch_code'].map(branch_code_map)
 df_results = df_results[['buy_user_id', 'orders_code', 'last_orders_date', 'last_order_cost', 
-                         'branch_code_chinese', 'branch_code', 'possibility']]
+                         'branch_code_chinese', 'branch_code', 'dept_code_cn', 'possibility']]
 
 
 # buy_user_id,branch_code,possibility,orders_code,last_orders_date,last_order_cost
@@ -36,12 +36,15 @@ df_results = df_results[['buy_user_id', 'orders_code', 'last_orders_date', 'last
 df_results.rename(columns={'branch_code': '科室代码', 'branch_code_chinese': '科室名', 
                            'buy_user_id': '客户编码', 'possibility': '模型输出概率',
                            'orders_code': '订单编号', 'last_orders_date': '最近购买时间',
-                           'last_order_cost': '购买金额'},
+                           'last_order_cost': '购买金额', 'dept_code_cn': '所属部门'},
                   inplace=True)
 
 # df_results.to_csv('./data/final_results_data_processed.csv', index=False, sep='\t')
 
 df_results.to_excel('./data/final_results_data_processed_new.xlsx', index=False)
+
 df_results[:500].to_excel('./data/final_results_data_processed_new_top500.xlsx', index=False)
+
+df_results[:1000].to_excel('./data/final_results_data_processed_new_top1000.xlsx', index=False)
 
 
